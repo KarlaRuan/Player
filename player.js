1368877a18f18b655c07b4502cbe5c29d25ed1d2
Karla <karla.ruan@cceo.com.mx>
2018-05-10 11:13:54 -0500

    Intento de añadir barra de progreso

--- player.js
+++ player.js
@@ -10,10 +10,12 @@ class Video{
 		this.playPauseBtn=document.querySelector(this.selector + " .play-pause");
 		this.showVolumeBtn=document.querySelector(this.selector + " .show-volume");
 		this.volumeRange=document.querySelector(this.selector + " #volume-range");
+		this.progressBar=document.querySelector(this.selector + " .bar")
 
 		this.playPauseBtn.addEventListener('click',()=>this.playPause());
 		this.showVolumeBtn.addEventListener('click',()=>this.toggleVolume()); /*Crea funcion para aparecer y desaparecer icono*/
 		this.volumeRange.addEventListener('input',()=>this.updateVolume());
+		this.videoElement.addEventListener('timeupdate',()=>this.updateProgress())
 	}
 	playPause(){
 		if(this.videoElement.paused){
@@ -28,7 +30,7 @@ class Video{
 	toggleVolume(){
 		document.querySelector(this.selector + " .volume").classList.toggle('active');	
 	}
-	updateVolume(){
+	updateVolume(){  //Función para cambiar de iconos dependiendo del volumen
 		if(this.volumeRange.value==0){
 			this.showVolumeBtn.innerHTML="volume_mute";
 		}else if(this.volumeRange.value<.5){
@@ -37,5 +39,14 @@ class Video{
 			this.showVolumeBtn.innerHTML="volume_up";
 		}
 		this.videoElement.volume=this.volumeRange.value;
+	}
+	updateProgress(){
+		let currentTime=this.videoElement.currentTime; //Guardarmos lo que lleva el video
+		let fullTime=this.videoElement.duracion; //Guardarmos la duracion total del video
+		/*Regla de 3 para obtener cuanto se irá recorriendo la barra de progreso
+		fullTime	 100
+		currentTime  x    */
+		let percentage= Math.floor((currentTime * 100) / fullTime); //Mah.floor es para redondear
+		this.progressBar.style.width=percentage+"%";  //Actualizar el ancho de la barra de progreso
 	}
 }
